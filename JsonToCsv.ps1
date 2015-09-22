533ce20b55213b294902960067e1476735cfcaf9
Stan <sdotloe@gmail.com>
2015-09-22 11:01:42 -0700

    add Powershell file to repo

---
+++ JsonToCsv.ps1
@@ -0,0 +1,8 @@
+﻿$path = 'C:\Users\stan\Downloads\Datasets\Github\AllGithubCommentsOnIssuesForD3\AllGithubCommentsOnIssuesForD3\'
+$files = Get-ChildItem $path -join '*.json'
+
+ForEach ($file in $files) { 
+    $psObj = (Get-Content $file -Raw | ConvertFrom-Json) | Convertto-CSV -NoTypeInformation
+    Write-Output $file
+    $psObj | Out-File $path -join 'csv2.txt' -Append
+}
